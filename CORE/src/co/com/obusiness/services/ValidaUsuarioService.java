18442beee40632d6fae8e11d886260d24a17fdae
hossa2016 <hernosa@gmail.com>
2020-06-29 11:27:19 -0500

    Delete ValidaUsuarioService.java

--- CORE/src/co/com/obusiness/services/ValidaUsuarioService.java
+++
@@ -1,8 +0,0 @@
-package co.com.obusiness.services;
-
-import co.com.obusiness.entidades.TsEmpUsuarios;
-
-public interface ValidaUsuarioService {
-	
-	public String validaUsuario(TsEmpUsuarios usuario) throws Exception;
-}
